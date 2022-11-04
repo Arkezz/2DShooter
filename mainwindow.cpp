ed4c74f6052fe3e48a3d274bb854ba09ba6531d1
= <40185507+Arkezz@users.noreply.github.com>
2022-11-04 17:46:18 +0200

    Naming Refactor and system for mapping .tmx

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 
-//Pixel size of each block in the grid
-const int blockLen = 32;
+//Pixel size of each Tile in the grid
+const int tileLen = 32;
 
 MainWindow::MainWindow(QWidget* parent)
 	: QMainWindow(parent)
@@ -27,8 +27,8 @@ MainWindow::~MainWindow()
 void MainWindow::addTile(int x, int y, QString tileName) {
 	QGraphicsPixmapItem* newItem = new QGraphicsPixmapItem;
 	//Set pixmap from .qrc file and make sure it is scaled
-	newItem->setPixmap(QPixmap(":/tiles/" + tileName).scaled(blockLen, blockLen));
-	newItem->setPos(x * blockLen, y * blockLen);
+	newItem->setPixmap(QPixmap(":/tiles/" + tileName).scaled(tileLen, tileLen));
+	newItem->setPos(x * tileLen, y * tileLen);
 	scene->addItem(newItem);
 }
 
@@ -36,26 +36,31 @@ void MainWindow::addTile(int x, int y, QString tileName) {
 void MainWindow::drawScene() {
 	scene->clear();
 
-	//Read the text file level 1 from .qrc
-	QFile file(":/maps/level1");
-	if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-		return;
-	//Draw the tilemap according to the file contents 1 is grass 2 is wall
+	//Read the tmx file then go line by line in the csv
+	QFile file(":/maps/map1");
+	file.open(QIODevice::ReadOnly);
 	QTextStream in(&file);
-	int y = 0;
-	while (!in.atEnd()) {
-		QString line = in.readLine();
-		for (int x = 0; x < line.length(); x++) {
-			if (line[x] == '1') {
-                addTile(x, y, "grass");
-			}
-			else if (line[x] == '2') {
-                addTile(x, y, "wall");
-			}
-            else {
+	QString line = in.readLine();
+	while (!line.isNull()) {
+		if (line.contains("<data encoding=\"csv\">")) {
+			line = in.readLine();
+			int y = 0;
+			while (!line.isNull()) {
+				QStringList list = line.split(",");
+				for (int x = 0; x < list.size(); x++) {
+					//If the tile is not empty, add it to the scene
+					if (list[x] == "0") {
+						addTile(x, y, "grass");
+					}
+					else if (list[x] == "1") {
+						addTile(x, y, "wall");
+					}
+				}
+				y++;
+				line = in.readLine();
 			}
 		}
-		y++;
+		line = in.readLine();
 	}
 }
 
@@ -68,8 +73,8 @@ void MainWindow::createScene() {
 
 //Size changes
 void MainWindow::setSize() {
-    this->setFixedSize(960, 720);
+	this->setFixedSize(960, 720);
 	int x = ((screenGeometry.width() - this->width()) / 2);
 	int y = ((screenGeometry.height() - this->height()) / 2);
-    this->move(x, y);
+	this->move(x, y);
 }
